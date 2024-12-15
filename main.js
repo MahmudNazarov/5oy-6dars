c6785e03630784a836a21e51c804df9241c87b29
MahmudNazarov <sh.nazarov@gmail.com>
2024-12-15 14:25:24 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const user = prompt("Iltimos tanlang: \n 1) pop \n 2) shift")
 const oquvchilar = ["mahmud" , "hayot" , "murod" , "ismoil"]
 if (user == 1) {
-    console.log((oquvchilar.pop()));
-    alert(oquvchilar)
+    alert(`siz ${oquvchilar.pop()} ismini olib tashadis`)
 } else if (user == 2) {
-    console.log((oquvchilar.shift()));
-    alert(oquvchilar)
+    alert(`siz ${oquvchilar.shift()} ismini olib tashadis`)
+} else{
+ alert(`siz ${oquvchilar} bu ismlardan hechnima oplibtashamadis`)
 }
